1860c6fe16f66148ad2b7b67ae014e56e9cae575
Carol <lxw@cnkj.site>
2023-09-01 15:16:43 +0800

    add: 自定义站点地址

--- src/main/java/run/halo/sitemap/SitemapPluginConfig.java
+++ src/main/java/run/halo/sitemap/SitemapPluginConfig.java
@@ -5,6 +5,7 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 
 import java.net.MalformedURLException;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
@@ -14,6 +15,7 @@ import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.Exceptions;
 import run.halo.app.infra.ExternalUrlSupplier;
+import run.halo.app.plugin.SettingFetcher;
 import run.halo.app.plugin.ReactiveSettingFetcher;
 
 import java.net.MalformedURLException;
@@ -23,10 +25,23 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 @Component
-@AllArgsConstructor
 public class SitemapPluginConfig {
 
     private final ExternalUrlSupplier externalUrlSupplier;
+
+    private final SettingFetcher settingFetcher;
+
+    private final BaseSetting baseSetting;
+
+
+    public SitemapPluginConfig(ExternalUrlSupplier externalUrlSupplier,
+        SettingFetcher settingFetcher) {
+        this.externalUrlSupplier = externalUrlSupplier;
+        this.settingFetcher = settingFetcher;
+        this.baseSetting = this.settingFetcher.fetch(BaseSetting.GROUP, BaseSetting.class)
+            .orElseGet(BaseSetting::new);
+    }
+
 
     private final DefaultSettingFetcher settingFetcher;
 
@@ -49,8 +64,22 @@ public class SitemapPluginConfig {
                 }
                 SitemapGeneratorOptions options;
                 try {
+                String siteUrl = baseSetting.getSiteUrl();
+
+                SitemapGeneratorOptions options;
+                try {
+                    URL url;
+                    if (StringUtils.isNotEmpty(siteUrl)) {
+                        url = new URL(siteUrl);
+                    } else {
+                        var uri = externalUrlSupplier.get();
+                        if (!uri.isAbsolute()) {
+                            uri = request.exchange().getRequest().getURI().resolve(uri);
+                        }
+                        url = uri.toURL();
+                    }
                     options = SitemapGeneratorOptions.builder()
-                        .siteUrl(uri.toURL())
+                        .siteUrl(url)
                         .build();
                 } catch (MalformedURLException e) {
                     throw Exceptions.propagate(e);
