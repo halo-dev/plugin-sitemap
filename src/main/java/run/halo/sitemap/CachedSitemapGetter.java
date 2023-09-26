6c92275cbe9b217cf7f72b832be5312e3aae5116
guqing <i@guqing.email>
2023-09-26 16:20:31 +0800

    refactor: cache time

--- src/main/java/run/halo/sitemap/CachedSitemapGetter.java
+++ src/main/java/run/halo/sitemap/CachedSitemapGetter.java
@@ -17,7 +17,7 @@ public class CachedSitemapGetter {
         .concurrencyLevel(Runtime.getRuntime().availableProcessors())
         .initialCapacity(8)
         .maximumSize(8)
-        .expireAfterWrite(Duration.ofDays(1))
+        .expireAfterWrite(Duration.ofSeconds(30))
         .build();
 
     private final DefaultSitemapEntryLister lister;
